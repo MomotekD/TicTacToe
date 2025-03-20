8f5b226036512846772e795bcfcdbffeaf15eb8d
MomotekD <86006101+MomotekD@users.noreply.github.com>
2025-03-20 23:20:50 +0200

    Add: score display functionality and reset score logic

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ const gameBoard = {
     setPlayers: function (name1, name2){
         this.player1 = this.player(name1 || "Player 1", "X");
         this.player2 = this.player(name2 || "Player 2", "O");
+
+        this.scoreDisplay();
     },
     player: function player(Name, Symbol){
         const name = Name;
@@ -29,17 +31,13 @@ const gameBoard = {
         let score = 0;
         const getScore = () => score;
         const giveScore = () => score++;
+        const resetScore = () => score = 0;
     
-        return {name, symbol, getScore, giveScore}
+        return {name, symbol, getScore, giveScore, resetScore}
     },
     playGame: function(index){
         if(this.winner != null){
             console.log('the game is finished')
-            return
-        }
-        if(this.cells.every(cell => cell !== '')){
-            console.log("It's a draw! ");
-            this.nextRound();
             return
         }
         if(this.cells[index] != ''){
@@ -55,6 +53,11 @@ const gameBoard = {
             console.log(`${winner} wins!`);
             this.nextRound();
             this.winner = null;
+            return
+        }
+        if(this.cells.every(cell => cell !== '')){
+            console.log("It's a draw! ");
+            this.nextRound();
             return
         }
 
@@ -90,21 +93,40 @@ const gameBoard = {
         }else{
             console.log('starting new game...')
         }
+        this.scoreDisplay();
         this.displayBoard();
     },
     displayBoard: function(){
         const board = document.querySelector('.board');
+        if(this.player1 === null){
+            this.player1 = this.player("Player 1", "X");
+            this.player2 = this.player("Player 2", "O");
+        }
+        this.scoreDisplay();
         board.innerHTML = '';
         this.cells = ['', '', '', '', '', '', '', '', ''];
         this.cells.forEach((cell, i) => {
             const Cell = document.createElement("button");
             Cell.classList.add("cell");
             Cell.addEventListener("click", () => {
-                Cell.textContent = `${this.currentPlayer}`
+                Cell.textContent = `${this.currentPlayer}`;
                 this.playGame(i);
-            });
+
+            }, { once: true });
             board.appendChild(Cell)   
         });
+    },
+    scoreDisplay: function(){
+        const player1Label = document.querySelector('.player1Score');
+        const player2Label = document.querySelector('.player2Score');
+
+        player1Label.textContent = `${this.player1.name} score: ${this.player1.getScore()}`;
+        player2Label.textContent = `${this.player2.name} score: ${this.player2.getScore()}`;
+    },
+    reset: function(){
+        this.player1.resetScore();
+        this.player2.resetScore();
+        this.displayBoard();
     }
 }
 
